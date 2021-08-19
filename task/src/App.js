936018a52ae34e68dda9b804c9fa9ad44e6145db
ARAVINTHAN J <75514530+Aravinthan-J@users.noreply.github.com>
2021-08-19 16:05:20 +0530

    task

---
+++ task/src/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="outerContainer">
+      <div className="leftContainer">
+        <ul>
+          <li>Home</li>
+          <li>Tap Menu</li>
+          <li>Draft List</li>
+          <li>Hours</li>
+        </ul>
+      </div>
+      <div className="upContainer">Myself am Aravinthan From DWP Team</div>
+      <div className="middleContainer">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged
+      </div>
+      <div className="bottomContainer">Thank You All</div>
+    </div>
+  );
+}
